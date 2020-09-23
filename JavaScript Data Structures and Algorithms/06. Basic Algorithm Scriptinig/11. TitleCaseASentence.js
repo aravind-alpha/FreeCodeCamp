d0609748a8491b8cc56f0f2f9f2b89446f949ad1
aravind-alpha <s.aravindviews@gmail.com>
2020-09-23 08:19:06 +0530

    Basic Algorithm Scripting Solutions

--- JavaScript Data Structures and Algorithms/06. Basic Algorithm Scriptinig/11. TitleCaseASentence.js
+++ JavaScript Data Structures and Algorithms/06. Basic Algorithm Scriptinig/11. TitleCaseASentence.js
@@ -1,3 +1,19 @@
+// Using Regex :
+function titleCase(str) {
+  return str
+    .toLowerCase()
+    .replace(/(^|\s)\S/g, (letter) => letter.toUpperCase());
+}
+
+// Using Map :
+function titleCase(str) {
+  let lowerStr = str.toLowerCase().split(" ");
+  let replacedChar = lowerStr.map(function (char) {
+    return char.replace(char[0], char[0].toUpperCase());
+  });
+  return replacedChar.join(" ");
+}
+
 // Using Map and charAt() :
 function titleCase(str) {
   let lowerStr = str.toLowerCase().split(" ");
@@ -5,13 +21,6 @@ function titleCase(str) {
     return char.replace(char.charAt(0), char.charAt(0).toUpperCase());
   });
   return replacedChar.join(" ");
-}
-
-// Using Regex :
-function titleCase(str) {
-  return str
-    .toLowerCase()
-    .replace(/(^|\s)\S/g, (letter) => letter.toUpperCase());
 }
 
 titleCase("I'm a little tea pot");
