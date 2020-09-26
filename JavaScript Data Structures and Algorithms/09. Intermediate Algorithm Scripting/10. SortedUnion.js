02797cb63dada6d5205793f9fd4aeab0e0f357fa
aravind-alpha <s.aravindviews@gmail.com>
2020-09-26 20:40:37 +0530

    Intermediate Algorithm Scripting Solutions

--- JavaScript Data Structures and Algorithms/09. Intermediate Algorithm Scripting/10. SortedUnion.js
+++ JavaScript Data Structures and Algorithms/09. Intermediate Algorithm Scripting/10. SortedUnion.js
@@ -1,1 +1,10 @@
 // Sorted Union
+
+/* 
+* Function that takes two or more arrays and returns a new array of unique values in the order 
+  of the original provided arrays. */
+
+function uniteUnique(...arrays) {
+  let newArr = [].concat(...arrays);
+  return [...new Set(newArr)];
+}
