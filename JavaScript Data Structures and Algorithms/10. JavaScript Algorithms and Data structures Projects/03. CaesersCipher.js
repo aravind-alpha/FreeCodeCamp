f27afb6384275347861b5c0566da39c898ee4d4f
aravind-alpha <s.aravindviews@gmail.com>
2020-09-27 16:01:34 +0530

    JS Algorithm and Data structures Projects Solutions

---
+++ JavaScript Data Structures and Algorithms/10. JavaScript Algorithms and Data structures Projects/03. CaesersCipher.js
@@ -0,0 +1,25 @@
+// Caesars Cipher
+
+/* 
+* One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. 
+  In a shift cipher the meanings of the letters are shifted by some set amount.
+
+* A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. 
+  Thus 'A' ↔ 'N', 'B' ↔ 'O' and so on.
+
+* Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+* All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), 
+  but do pass them on. */
+
+function rot13(str) {
+  return str.replace(/[A-Z]/g, (letter) =>
+    String.fromCharCode((letter.charCodeAt(0) % 26) + 65)
+  );
+}
+/* 
+* = NOTE = 
+* Mapping a range of 26 numbers i.e. between [65 - 90] which represents uppercase [English alphabets] 
+  in Unicode character set 74 to a range of numbers between [0 - 25]. */
+
+rot13("SERR PBQR PNZC");
