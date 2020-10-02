86b29b27c894b6b6c85b5b7049581953f7a8b6b3
aravind-alpha <s.aravindviews@gmail.com>
2020-10-02 12:08:49 +0530

    Updated Theme and Styling

--- Responsive Web Design/01. Tribute Page/js/script.js
+++ Responsive Web Design/01. Tribute Page/js/script.js
@@ -2,3 +2,7 @@ function darkModeToggle() {
   let element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+function swapStyle(sheet) {
+  document.getElementById("mystylesheet").href = sheet;
+}
