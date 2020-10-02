9f927a6bbdd2ac34e90c98feb992c64c39007c98
aravind-alpha <s.aravindviews@gmail.com>
2020-10-02 19:38:09 +0530

    Completed Tribute Page - FCC

--- Responsive Web Design/01. Tribute Page/js/script.js
+++ Responsive Web Design/01. Tribute Page/js/script.js
@@ -1,4 +1,14 @@
-// Light / Dark mode Switch
+// Light | Dark mode Saving in Local storage
+
+var setTheme = localStorage.getItem("theme");
+
+if (setTheme == null) {
+  swapStyle("light.css");
+} else {
+  swapStyle(setTheme);
+}
+
 function swapStyle(sheet) {
   document.getElementById("mystylesheet").href = sheet;
+  localStorage.setItem("theme", sheet);
 }
